996dbd2466057961227596431848f0f30077f1b8
Uriel Deveaud <uriel.deveaud@gmail.com>
2021-11-04 14:24:54 -0600

    Create track_sp23.rb

---
+++ Music Tracks/In Progress/track_sp23.rb
@@ -0,0 +1,73 @@
+
+bpm = 160
+
+# ############ ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### #####
+#             o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o
+# |||||||||||||   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   ||||||
+
+pattern =    "9---8---8---9---9---8---8---9-7-9---8---8---9---9---8---8---9-7-".ring
+pattern_b =  "9-8-8---8-5---9-----2---5---9-7-9-8-8---8-5---9-----2---5---9-7-".ring
+
+pattern2 =   "--2--12---1---11--2--12---1---11".ring
+pattern3 =   "9--8--8--9--".ring
+pattern4 =   "---8--------9-------8-------9---".ring
+
+# ##################################################################################
+
+with_fx :distortion, mix: 0.5 do
+  live_loop :kick do
+    use_bpm bpm
+    3.times do
+      pattern.length.times do
+        sample :bd_haus, rate: 0.5 ,  amp: (pattern[look].to_f / 9) if (pattern[tick] != "-")
+        sleep 8/pattern.length.to_f
+      end
+    end
+    tick_reset
+    pattern_b.length.times do
+      sample :bd_haus, rate: 0.5 ,  amp: (pattern_b[look].to_f / 9) if (pattern_b[tick] != "-")
+      sleep 8/pattern_b.length.to_f
+    end
+    tick_reset
+  end
+end
+
+
+live_loop :sn do
+  
+  use_bpm bpm
+  pattern2.length.times do
+    sample :drum_cymbal_closed, rate: 0.3, amp: (pattern2[look].to_f / 9) if (pattern2[tick] != "-")
+    sleep 8/pattern2.length.to_f
+  end
+  tick_reset
+end
+
+live_loop :dr do
+  
+  use_bpm bpm
+  pattern3.length.times do
+    sample :bd_fat, amp: (pattern3[look].to_f / 9) if (pattern3[tick] != "-")
+    sleep 3/pattern3.length.to_f
+  end
+  tick_reset
+end
+
+live_loop :snares do
+  
+  use_bpm bpm
+  pattern4.length.times do
+    sample :sn_zome, cutoff: 100, amp: (pattern4[look].to_f / 10) if (pattern4[tick] != "-")
+    sleep 8/pattern4.length.to_f
+  end
+  tick_reset
+end
+
+live_loop :a do
+  use_synth :prophet
+  n = ring(45, 56, 62, 55).tick
+  play n, amp: 0.15, release: rrand(0.3, 0.8), sustain: rrand(0.2, 1.2), attack: rrand(0.1, 0.5)
+  sleep 1
+end
+
+
